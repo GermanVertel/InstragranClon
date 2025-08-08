36bbe7977ec914f6f3f7974f1738db7404769037
GermanVertel <84603164+GermanVertel@users.noreply.github.com>
2025-08-08 15:37:44 -0500

    creacion del TabView principal

--- InstragranClon/Core/TabBar/MainTabview.swift
+++ InstragranClon/Core/TabBar/MainTabview.swift
@@ -9,6 +9,32 @@ import SwiftUI
 
 struct MainTabview: View {
     var body: some View {
+        TabView{
+            Text("Feed")
+                .tabItem{
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem{
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem{
+                    Image(systemName: "plus.square")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
     
     }
 }
